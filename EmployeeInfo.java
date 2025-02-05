6cc15e17681c5c5acc0c439043b5c4263af61572
Jyoti Vashisth <jvashisth1999@gmail.com>
2025-02-05 22:18:51 +0530

    Added EmployeeInfo.java

---
+++ EmployeeInfo.java
@@ -0,0 +1,10 @@
+public class EmployeeInfo {
+    public static void main(String[] args) {
+        System.out.println("NAME\tMAIL-ID\t\t\tEMPLOYEE-CODE\tPHONE");
+        System.out.println("Parul\tparul@cse\tp03161\t\t25764728");
+        System.out.println("Sonia\tsonia@cse\tp03160\t\t25764728");
+        System.out.println("Rahul\trahul@cse\tp03162\t\t25764729");
+        System.out.println("Meera\tmeera@cse\tp03163\t\t25764730");
+        System.out.println("Amit\tamit@cse\tp03164\t\t25764731");
+    }
+}
