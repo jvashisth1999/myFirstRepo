f649fa33c3c5ad0a62c66e11f5f901ab9f638164
jvashisth1999 <jvashisth1999@gmail.com>
2025-02-15 11:20:10 +0530

    EmployeeInfo.java

--- EmployeeInfo.java
+++ EmployeeInfo.java
@@ -1,10 +1,10 @@
 public class EmployeeInfo {
     public static void main(String[] args) {
-        System.out.println("NAME\tMAIL-ID\t\t\tEMPLOYEE-CODE\tPHONE");
-        System.out.println("Parul\tparul@cse\tp03161\t\t25764728");
-        System.out.println("Sonia\tsonia@cse\tp03160\t\t25764728");
-        System.out.println("Rahul\trahul@cse\tp03162\t\t25764729");
-        System.out.println("Meera\tmeera@cse\tp03163\t\t25764730");
-        System.out.println("Amit\tamit@cse\tp03164\t\t25764731");
+        System.out.println("NAME\tMAIL-ID\t\tEMPLOYEE-CODE\tPHONE");
+        System.out.println("Saurav\tsaurav@gmail.com\tp0010\t\t8929619594");
+        System.out.println("Komal\tkomal@gmail.com\tp0002\t\t9311808932");
+        System.out.println("Jyoti\tjyoti@gmail.com\tp0003\t\t9729925828");
+        System.out.println("Nikita\tnikita@gmail.com\tp0004\t\t9053582912");
+        System.out.println("Sakshi\tsakshi@gmail.com\tp0011\t\t9350728499");
     }
 }
