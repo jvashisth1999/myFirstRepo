7955f169a3e02716bcbb03926adfbf167a47caa8
jvashisth1999 <jvashisth1999@gmail.com>
2025-02-26 11:21:42 +0530

    Add files via upload

---
+++ TextArray.java
@@ -0,0 +1,8 @@
+class TextArray {
+    public static void main(String[] args) {
+        String[] names = {"Jyoti", "Komal", "Saurav", "Nikita", "Sakshi"};
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
