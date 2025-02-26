0962d0cec0b6832cb08f508d3fb182b332d39340
jvashisth1999 <jvashisth1999@gmail.com>
2025-02-26 12:25:35 +0530

    Operators.java

--- Operators.java
+++ Operators.java
@@ -1,83 +1,95 @@
-public class Operators {
-    public static void main(String[] args) {
-        
-        int a = 10, b = 3;
+import java.util.Scanner;
 
-        // Arithmetic Operators
-        System.out.println("Arithmetic Operators:");
-        System.out.println("a + b = " + (a + b));
-        System.out.println("a - b = " + (a - b));
-        System.out.println("a * b = " + (a * b));
-        System.out.println("a / b = " + (a / b));
-        System.out.println("a % b = " + (a % b));
+class Operator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number (a): ");
+        int a = scanner.nextInt();
+        System.out.print("Enter second number (b): ");
+        int b = scanner.nextInt();
         
+        System.out.println();
+
+        // Arithmetic operators
+        System.out.println("Arithmetic Operators:");
+        System.out.println("Addition of a and b is: " + (a + b));
+        System.out.println("Subtraction of a and b is: " + (a - b));
+        System.out.println("Multiplication of a and b is: " + (a * b));
+        System.out.println("Division of a and b is: " + (a / b));
+        System.out.println("Remainder of a and b is: " + (a % b));
+        System.out.println();
+
         // Relational Operators
-        System.out.println("\nRelational Operators:");
-        System.out.println("a == b: " + (a == b));
-        System.out.println("a != b: " + (a != b));
-        System.out.println("a > b: " + (a > b));
-        System.out.println("a < b: " + (a < b));
-        System.out.println("a >= b: " + (a >= b));
-        System.out.println("a <= b: " + (a <= b));
+        System.out.println("Relational Operators:");
+        System.out.println("A is greater than B: " + (a > b));
+        System.out.println("A is less than B: " + (a < b));
+        System.out.println("A is equal to B: " + (a == b));
+        System.out.println("A is not equal to B: " + (a != b));
+        System.out.println();
 
         // Logical Operators
-        boolean x = true, y = false;
-        System.out.println("\nLogical Operators:");
-        System.out.println("x && y: " + (x && y));
-        System.out.println("x || y: " + (x || y));
-        System.out.println("!x: " + !x);
+        System.out.println("Logical Operators:");
+        System.out.print("Enter boolean value for c (true/false): ");
+        boolean c = scanner.nextBoolean();
+        System.out.print("Enter boolean value for d (true/false): ");
+        boolean d = scanner.nextBoolean();
+        System.out.println("And Operator (c && d): " + (c && d));
+        System.out.println("Or Operator (c || d): " + (c || d));
+        System.out.println("Not Operator (!c): " + (!c));
+        System.out.println();
 
         // Bitwise Operators
-        System.out.println("\nBitwise Operators:");
-        //int p = 5, q = 3; // 0101 & 0011
-        System.out.println("a & b = " + (a & b));  // Bitwise AND
-        System.out.println("a | b = " + (a | b));  // Bitwise OR
-        System.out.println("a ^ b = " + (a ^ b));  // Bitwise XOR
-        System.out.println("~a = " + (~a));        // Bitwise NOT
-        System.out.println("a << 1 = " + (a << 1)); // Left shift
-        System.out.println("a >> 1 = " + (a >> 1)); // Right shift
-        System.out.println("a >>> 1 = " + (a >>> 1)); // Unsigned right shift
+        System.out.println("Bitwise Operators:");
+        System.out.print("Enter first binary number (e in decimal): ");
+        int e = scanner.nextInt();
+        System.out.print("Enter second binary number (f in decimal): ");
+        int f = scanner.nextInt();
+        System.out.println("AND Operator (e & f): " + (e & f));
+        System.out.println("OR Operator (e | f): " + (e | f));
+        System.out.println();
 
         // Assignment Operators
-        System.out.println("\nAssignment Operators:");
-        int num = 10;
-        num += 5;
-        System.out.println("num += 5: " + num); // num = num + 5
-        num -= 3;
-        System.out.println("num -= 3: " + num); // num = num - 3
-        num *= 2;
-        System.out.println("num *= 2: " + num); // num = num * 2
-        num /= 4;
-        System.out.println("num /= 4: " + num); // num = num / 4
-        num %= 3;
-        System.out.println("num %= 3: " + num); // num = num % 3
+        System.out.println("Assignment Operators:");
+        System.out.println("Assignment of A (a += 3): " + (a += 3));
+        System.out.println("Assignment of B (b += 1): " + (b += 1));
+        System.out.println("Assignment of A (a -= 3): " + (a -= 3));
+        System.out.println("Assignment of B (b -= 1): " + (b -= 1));
+        System.out.println();
 
-        // Unary Operators
-        System.out.println("\nUnary Operators:");
-        System.out.println("++a: " + (++a)); // Pre-increment
-        System.out.println("a++: " + (a++)); // Post-increment
-        System.out.println("a after a++: " + a); // After post-increment
-        System.out.println("--a: " + (--a)); // Pre-decrement
-        System.out.println("a--: " + (a--)); // Post-decrement
-        System.out.println("a after a--: " + a); // After post-decrement
-        System.out.println("~a: " + (~a)); // Bitwise NOT
-        System.out.println("!x: " + !x); // Logical NOT
+        // Unary operators
+        System.out.println("Unary Operators:");
+        System.out.println("Post Increment of A: " + (++a));
+        System.out.println("Pre Increment of A: " + (a++));
+        System.out.println("Post Decrement of B: " + (--b));
+        System.out.println("Pre Decrement of B: " + (b--));
+        System.out.println();
 
         // Ternary Operator
-        System.out.println("\nTernary Operator:");
+        System.out.println("Ternary Operator:");
         String result = (a > b) ? "a is greater" : "b is greater";
         System.out.println(result);
+        System.out.println();
 
         // Instanceof Operator
-        System.out.println("\nInstanceof Operator:");
+        System.out.println("Instanceof Operator:");
         String str = "Hello";
         System.out.println("str instanceof String: " + (str instanceof String));
-        
-        // Type Cast Operator
-        System.out.println("\nType Cast Operator:");
-        double d = 10.5;
-        int castedValue = (int) d; // Casting from double to int
-        System.out.println("Casted value (double to int): " + castedValue);
+        System.out.println();
 
+        // Shift Operators
+        System.out.println("Shift Operator:");
+        System.out.println("A shift left (a << 1): " + (a << 1));
+        System.out.println("A shift right (a >> 1): " + (a >> 1));
+        System.out.println();
+
+        // Type Cast Operator
+        System.out.println("Type Cast Operator:");
+        System.out.print("Enter an integer value to type cast: ");
+        int y = scanner.nextInt();     
+        float u = y;
+        double i = u;
+        System.out.println("After Type Casting: " + u);
+
+        scanner.close();
     }
 }
